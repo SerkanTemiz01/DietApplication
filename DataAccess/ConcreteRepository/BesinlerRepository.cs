e53d991433fd02e7f37b01691539766dd9e9384a
fatih <fatih@DESKTOP-B0BPUKI>
2022-11-16 01:13:06 +0300

    ConcreteRepository

---
+++ DataAccess/ConcreteRepository/BesinlerRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ConcreteRepository
+{
+    public class BesinlerRepository : BaseRepository<Besinler>
+    {
+        public BesinlerRepository(DietQueryDBContext dietQueryDBContext) : base(dietQueryDBContext)
+        {
+        }
+    }
+}
---
+++ DataAccess/ConcreteRepository/KullaniciBilgisiRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ConcreteRepository
+{
+    public class KullaniciBilgisiRepository : BaseRepository<KullaniciBilgisi>
+    {
+        public KullaniciBilgisiRepository(DietQueryDBContext dietQueryDBContext) : base(dietQueryDBContext)
+        {
+        }
+    }
+}
